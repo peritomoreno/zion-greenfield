9f0fbad9c8a1af4be8f8c796177f76a4f26af8ab
Vincent Chen <vc88086@gmail.com>
2020-03-26 10:39:28 -0700

    implement product API

--- client/src/api/product.js
+++ client/src/api/product.js
@@ -1,3 +1,19 @@
-const Product = {};
+const Product = {
+  getProduct: (id) => {
+    return fetch(`http://3.134.102.30/products/${id}`).then((res) => {
+      return res.json();
+    });
+  },
+  getStyles: (id) => {
+    return fetch(`http://3.134.102.30/products/${id}/styles`).then((res) => {
+      return res.json();
+    });
+  },
+  getRelated: (id) => {
+    return fetch(`http://3.134.102.30/products/${id}/related`).then((res) => {
+      return res.json();
+    });
+  }
+};
 
 export default Product;
