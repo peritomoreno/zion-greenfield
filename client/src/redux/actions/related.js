f1642e86caa8b24819394b54909af9c1d3f67605
Vincent Chen <vc88086@gmail.com>
2020-03-27 04:55:59 -0700

    fixed name of  related product action

--- client/src/redux/actions/related.js
+++ client/src/redux/actions/related.js
@@ -1,6 +1,6 @@
-const setCurrentProduct = (product) => ({
+const setRelatedProduct = (product) => ({
   type: 'SET_RELATED_PRODUCT',
   payload: product
 });
 
-export default setCurrentProduct;
+export default setRelatedProduct;
