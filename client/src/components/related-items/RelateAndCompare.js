c83d0635c911bc9f3e2609c3d1593573085b92b0
Vincent Chen <vc88086@gmail.com>
2020-03-28 11:36:16 -0700

    In progress of implementing thunk

--- client/src/components/related-items/RelateAndCompare.js
+++ client/src/components/related-items/RelateAndCompare.js
@@ -4,12 +4,20 @@ import RelatedProducts from './relatedProducts/RelatedProducts';
 import YourOutfit from './yourOutfit/YourOutfit';
 // import ComparisonModal from './comparisonModal/ComparisonModal';
 import '../../styles/RelatedProduct.css';
-import { setRelatedProduct } from '../../redux/actions/related';
+import {
+  setRelatedProduct,
+  getRelatedProduct
+} from '../../redux/actions/related';
 
 class RelateAndCompare extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+  }
+
+  componentDidMount() {
+    const { currentProduct, _getRelatedProduct } = this.props;
+    _getRelatedProduct(currentProduct.id);
   }
 
   render() {
@@ -26,10 +34,12 @@ class RelateAndCompare extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
-    relatedProducts: state.relatedProducts
+    relatedProducts: state.relatedProducts,
+    currentProduct: state.currentProduct
   };
 };
 
 export default connect(mapStateToProps, {
-  setRelatedProduct
+  _setRelatedProduct: setRelatedProduct,
+  _getRelatedProduct: getRelatedProduct
 })(RelateAndCompare);
--- client/src/redux/actions/related.js
+++ client/src/redux/actions/related.js
@@ -3,3 +3,17 @@ export const setRelatedProduct = (product) => ({
   type: SET_RELATED_PRODUCT,
   payload: product
 });
+
+export const GET_RELATED_PRODUCT = 'GET_RELATED_PRODUCT';
+export const getRelatedProduct = (currentProductId) => (dispatch) => {
+  // const relatedUrl = `http://3.134.102.30/products/${currentProductId}/related`;
+  // async fetch(relatedUrl)
+  //   .then((res) => {
+  //     return res.json();
+  //   })
+  //   .then((ids) =>
+  //     ids
+  //       .forEach((id) => fetch(`http://3.134.102.30/products/${id}`))
+  //       .then((res) => console.log(res.json()))
+  //   );
+};
