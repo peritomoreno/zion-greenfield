d8b9ef23f3dce97098e5f11e8d780dc90a6b61eb
Dan Silberger <dan.silberger@gmail.com>
2020-03-31 19:09:10 -0400

    style update in progress

--- client/src/components/reviews/productBreakdown/ProductBreakdown.js
+++ client/src/components/reviews/productBreakdown/ProductBreakdown.js
@@ -9,8 +9,15 @@ const ProductBreakdown = (data) => {
     <Col data-testid="productBreakdown" className="review-product-breakdown">
       {arr.map((rating) => (
         <Row>
-          <Col>{rating[0]}</Col>
-          <Col>{rating[1].value}</Col>
+          <label for={rating[0] + '-slider'}>{rating[0]}</label>
+          <input
+            type="range"
+            class="custom-range"
+            min="1"
+            max="5"
+            value={rating[1].value}
+            id={rating[0] + '-slider'}
+          />
         </Row>
       ))}
     </Col>
