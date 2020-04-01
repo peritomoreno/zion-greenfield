ab280ca069fdb23c0a4a4792890a2d85ab04afc5
Dan Silberger <dan.silberger@gmail.com>
2020-04-01 14:55:23 -0400

    grabbing new master stuff

--- client/src/components/reviews/productBreakdown/ProductBreakdown.js
+++ client/src/components/reviews/productBreakdown/ProductBreakdown.js
@@ -9,14 +9,14 @@ const ProductBreakdown = (data) => {
     <Col data-testid="productBreakdown" className="review-product-breakdown">
       {arr.map((rating) => (
         <Row>
-          <label for={rating[0] + '-slider'}>{rating[0]}</label>
+          <label htmlFor={`${rating[0]}-slider`}>{rating[0]}</label>
           <input
             type="range"
-            class="custom-range"
+            className="custom-range"
             min="1"
             max="5"
             value={rating[1].value}
-            id={rating[0] + '-slider'}
+            id={`${rating[0]}-slider`}
           />
         </Row>
       ))}
